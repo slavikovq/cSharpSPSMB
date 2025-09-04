b84cb2e1dbb55367d9125b5a95e27e7db82523d3
slavikovq <barbora.slavikova@student.spsmb.cz>
2025-09-04 11:01:31 +0200

    fizzbuzz

--- src/4rocnik/setup/setup/Program.cs
+++ src/4rocnik/setup/setup/Program.cs
@@ -1,9 +1,30 @@
+using System;
+
 namespace setup
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
