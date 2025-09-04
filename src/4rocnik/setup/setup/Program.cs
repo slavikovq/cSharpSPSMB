0d332373c968c8f0fb285a1f269d8f1cd96f43b3
slavikovq <barbora.slavikova@student.spsmb.cz>
2025-09-04 11:21:07 +0200

    number guess

--- src/4rocnik/setup/setup/Program.cs
+++ src/4rocnik/setup/setup/Program.cs
@@ -6,6 +6,7 @@ namespace setup
     {
         public static void Main(string[] args)
         {
+            /*
             for (int i = 1; i <= 100; i++)
             {
                 if (i % 3 == 0)
@@ -23,6 +24,29 @@ namespace setup
                 else
                 {
                     Console.WriteLine(i);
+                }
+            }
+            */
+            
+            bool gameRunning = true;
+            while (gameRunning)
+            {
+                Console.WriteLine("Write a number between 1 and 100.");
+                Random rnd = new Random();
+                int generatedNumber = rnd.Next(1, 100);
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                if (userNumber == generatedNumber)
+                {
+                    Console.WriteLine("You are the same number!");
+                    gameRunning = false;
+                }
+                else if(userNumber < 0 || userNumber > 100)
+                {
+                    Console.WriteLine("You don't have a number between 1 and 100!");
+                }
+                else
+                {
+                    Console.WriteLine("You are different numbers!");
                 }
             }
         }
