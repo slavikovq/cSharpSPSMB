fe3e5af52805420029065ea2b2d07daba168924a
slavikovq <barbora.slavikova@student.spsmb.cz>
2025-09-11 11:16:58 +0200

    Read list

---
+++ src/4rocnik/Maturita/Files/Movies.cs
@@ -0,0 +1,14 @@
+namespace Files
+{
+    class Movies
+    {
+        public string Film { get; set; }
+        public string Genre { get; set; }
+        public string LeadStudio {get; set;}
+        public int AudienceScore { get; set; }
+        public double Profitability  { get; set; }
+        public int RottenTomatoes { get; set; }
+        public double WorldwideGross {get; set;}
+        public int Year { get; set; }
+    }
+}
--- src/4rocnik/Maturita/Files/Program.cs
+++ src/4rocnik/Maturita/Files/Program.cs
@@ -1,9 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace Files
 {
   internal class Program
   {
     public static void Main(string[] args)
     {
+      List<Movies> movies = new List<Movies>();
+      StreamReader sr = new StreamReader("C:\\Users\\barbora.slavikova\\Desktop\\cSharpSPSMB\\src\\4rocnik\\Maturita\\Files\\movies.csv");
+
+      string line;
+      string[] item =  new string[8];
+      while ((line = sr.ReadLine()) != null)
+      { 
+        item = line.Split(',');
+        
+        movies.Add(new Movies
+        {
+          Film = item[0],
+          Genre = item[1],
+          LeadStudio = item[2],
+          AudienceScore = int.Parse(item[3]),
+          Profitability = double.Parse(item[4]),
+          RottenTomatoes = int.Parse(item[5]),
+          WorldwideGross = double.Parse(item[6]),
+          Year = int.Parse(item[7])
+        });
+      }
     }
   }
 }
